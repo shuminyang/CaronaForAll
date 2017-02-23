9117dbfe486e24c5e2fe16c9369147144d7f4415
Shu Min Yang Chang <shu.minyang@hotmail.com>
2017-02-23 19:19:37 -0300

    refazendo

--- README.md
+++
@@ -1,1 +0,0 @@
-# CaronaForAll
--- src/main/webapp/js/route.js
+++
@@ -1,6 +0,0 @@
-angular.module('caronaAll').config(function ($routeProvider) {
-    $routeProvider
-            .when('/teste', {
-                templateUrl: 'teste.html'
-            });
-});
