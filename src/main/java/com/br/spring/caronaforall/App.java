eb08f8cebdea0fcb143dc77cd22a7d4742cf6e5f
Shu Min Yang Chang <shu.minyang@hotmail.com>
2017-02-23 23:15:35 -0300

    commit

--- src/main/java/com/br/spring/caronaforall/App.java
+++ src/main/java/com/br/spring/caronaforall/App.java
@@ -1,24 +1,13 @@
 package com.br.spring.caronaforall;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner run() {
-
-        return (args) -> {
-            System.out.println(args);
-        };
-
     }
 
 }
